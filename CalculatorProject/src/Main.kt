958aed257d570dbb1ea45ff8506afbdfe8209b5a
Mei Rusfandi <mei.rusfandi@bankmandiri.co.id>
2022-03-03 07:50:30 +0700

    update and add grade

--- CalculatorProject/src/Main.kt
+++ CalculatorProject/src/Main.kt
@@ -1,51 +1,53 @@
+import kotlin.math.pow
+
 fun multiply(a: Int, b: Int): Int {
     return a * b
+}
+
+fun grades(number: Int, grade: Int): Int {
+    return number.toDouble().pow(grade.toDouble()).toInt()
+}
+
+fun readInput(msg: String): Int {
+    var masukan = 0
+    try {
+        print(msg)
+        masukan = readLine()!!.toInt()
+    } catch (e:Exception) {
+        println("tolong kasi integer")
+    }
+    return masukan
 }
 
 fun main() {
     println("Welcome to Calculator Apps!")
     var operation = -1
-    while (operation != 5) {
+    while (operation != 6) {
+        println()
+        println()
+        println("=============================================================")
         println("Please select the number of menu to use this calculator Apps!")
         println("1. Multiply Operation")
         println("2. Division Operation")
         println("3. Plus Operation")
-        println("4. Minus Operation")
-        println("5. Close Application")
-        while (true) {
-            try {
-                print("menu: ")
-                operation = readLine()!!.toInt()
-                break
-            } catch (e:Exception) {
-                println("tolong kasi integer")
-            }
-        }
-        var first:Int
-        if (operation >= 1 && operation <= 4) {
-            while (true) {
-                try {
-                    print("Input first number : ")
-                    first = readLine()!!.toInt()
-                    break
-                } catch (e:Exception) {
-                    println("tolong kasi integer")
-                }
-            }
+        println("4. Grade Operation (Bilangan Pangkat)")
+        println("5. Minus Operation")
+        println("6. Close Application")
+        println("=============================================================")
+        println()
+        println()
 
-            var second:Int
-            while (true) {
-                try {
-                    print("Input second number : ")
-                    second = readLine()!!.toInt()
-                    break
-                } catch (e:Exception) {
-                    println("tolong kasi integer")
-                }
-            }
+        operation = readInput("Choose menu : ")
+
+        var first:Int
+        var second:Int
+        if (operation in 1..5) {
+            first = readInput("Input first number : ")
+            second = readInput("Input second number : ")
+
             when (operation) {
                 1 -> {
-                    println(multiply(first, second))
+                    println("Result of multiply $first with $second is ${multiply(first, second)}")
                 }
                 2 -> {
                     // print first and second number and call the function
@@ -53,13 +55,15 @@ fun main() {
                 3 -> {
                     // print first and second number and call the function
                 }
-
+                4 -> {
+                    println("Result of grade value $first of $second is ${grades(first, second)}")
+                }
                 else -> {
                     // print first and second number and call the function
                 }
             }
         }
-
     }
+
     println("Thank you for using this calculator Apps")
 }
